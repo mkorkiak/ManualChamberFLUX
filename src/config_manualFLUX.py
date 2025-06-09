cfbe3f558e3b5fc841730df3143bb1bc0ff7407b
mkorkiak <64260757+mkorkiak@users.noreply.github.com>
2025-06-09 15:00:28 +0300

    Formatting and fixing typos

--- src/config_manualFLUX.py
+++ src/config_manualFLUX.py
@@ -8,6 +8,7 @@ Config file for the ManualChamberFlux program.
 #===============================================================================================================#
 
 #This is the name of the results file
+
 #The result file will have the following columns: Starting time of the measurement, source chamber, length of the
 #linear and exponential fits, headspace temperature, air pressure, linear and exponential fluxes of the gases,
 #and possible extra columns inserted at the end of the times file.
@@ -16,32 +17,33 @@ site_name='results_example'
 
 #===============================================================================================================#
 
-#Directory where fluxes are saved. A folder called 'plots' is created
-#into this folder, if plots are made.
+#Directory where fluxes are saved. 
+
+#A folder called 'plots' is created into this folder, if plots are made
 #The location needs to be in hyphens
 #The folders can be separated by "/" (Linux and windows) or by "\" (Windows)
-#But if the folders are separated by "\", one needs to add "r" before the first hyphen like this:
+#If the folders are separated by "\", one needs to add "r" before the first hyphen like this:
 #result_loc=r'C:\m\flux_example\results\'
 result_loc='/home/m/flux_example/results/'
 
 #===============================================================================================================#
 
 #The DIRECTORY where the daily data is located
+
 #The data file should be comma delimited (.csv).
 #Compressed (zip, tar.gz) csv files should also work.
-#The location has to be in hyphens
+#The location has to be in hyphens.
 
 #MANDATORY COLUMNS:
     #Datetime 
 
-#Include the gas mixing ratio columns below of which you want to calculate the
-#fluxes (these need to be DRY concentrations in PPM)
+#Include the gas mixing ratio columns below of which you want to calculate the fluxes 
+#(these need to be DRY concentrations in PPM)
 #You can also give headspace temperature, air pressure and PAR in this file. 
-#Any other columns included in the data file will be added to the result
-#file. So, include environmental values such as RH, soil temperatures, etc. here 
-#if you want their closure time averages saved.
-#If headspace temperature is given here AND also in the TIMES file, then the T
-#from the TIMES file will be used in the flux calculation.
+#Any other columns included in the data file will be added to the result file. So, include environmental values 
+#such as RH, soil temperatures, etc. here if you want their closure time averages saved.
+#If headspace temperature is given here AND also in the TIMES file, then the temperature from the TIMES file 
+#will be used in the flux calculation.
     #CO2_dry (ppm)
     #CH4_dry (ppm)
     #N2O_dry (ppm)
@@ -58,6 +60,7 @@ data_loc='/home/m/flux_example/data/data_example.csv'
 #===============================================================================================================#
 
 #This is the location of the "times" file (has to be an excel file ".xlsx")
+
 #MANDATORY COLUMNS (if not explicitly stated, the columns have to include 
 #sensible data, otherwise the program won't work):
     #Date
@@ -68,17 +71,20 @@ data_loc='/home/m/flux_example/data/data_example.csv'
     #Headspace temperature (C) (Leave empty if the T data is given in the chamber data file.)
     #Air pressure (hPa) (Not absolutely necessary, but the column has to exist. Leave empty if no data.)
 
-#If the data in the air pressure column is empty, the program will search for
-#air pressure data from the chamber data file, if no air pressure column is
-#found from these two sources, the flux is calculated without taking into
-#account the air pressure.
-#If headspace temperature is given here AND also in the DATA file, then
-#the T from the TIMES file will be used in the flux calculation.
+#Yes, it is enough to report the system height (no volume/surface area needed). HOWEVER, this assumes
+#that the chamber shape is either cylindrical or rectangular (volume / area = height). 
+#If, for some reason, you use a chamber of a different shape (please don't), be careful when calulcating the
+#system height.
+
+#If the data in the air pressure column is empty, the program will search for air pressure data in the chamber 
+#data file. If no air pressure data is found in either of these two sources, the flux is calculated without 
+#considering the air pressure.
+#If the headspace temperature is specified here and also in the DATA file, then the T value from the TIMES file 
+#will be used in the flux calculation.
 
 #Any other columns included in the times file will be added to the result
 #file. So, include environmental values like ST, WTD, SM measured during the
-#closure here if you want them saved
-#with the respective fluxes.
+#closure here if you want them saved with the respective fluxes.
 #The location needs to be in hyphens.
 #The folders can be separated by "/" (Linux and windows) or by "\" (Windows)
 #But if the folders are separated by "\", one needs to add "r" before the first hyphen like this:
@@ -88,6 +94,7 @@ times_loc='/home/m/flux_example/times_example.xlsx'
 #===============================================================================================================#
 
 #Time format of the DATA file
+
 #Example: year-month-day hour:minutes:seconds.decimals = '%Y-%m-%d %H:%M:%S.%f'
 #The time format needs to be in hyphens
 time_format_data='%Y-%m-%d %H:%M:%S.%f'
@@ -95,6 +102,7 @@ time_format_data='%Y-%m-%d %H:%M:%S.%f'
 #===============================================================================================================#
 
 #Time format of the TIMES file
+
 #Example: year-month-day hour:minutes:seconds = '%Y-%m-%d %H:%M:%S'
 #Note, if the user wants to interactively select the starting and ending times
 #by using this program, it is recommended to set the starting time earlier and
@@ -106,44 +114,43 @@ time_format_times='%Y-%m-%d %H:%M:%S'
 #===============================================================================================================#
 
 #Select starting and ending times of the measurements interactively
-#If True, then starting and ending times in the TIMES file are used to
-#plot the timeseries of the gases for each closure. For each plot, the
-#user has to left click the plot to select start and end times.
-#Selections can be cancelled by right clicking the plot.
-#This program assumes that the first click is the starting time and the second
-#click is the ending time and the third click closes the plot.
+
+#If True, then starting and ending times in the TIMES file are used to plot the time series of the gases for 
+#each closure. For each plot, the user must left-click the plot to select the start and end times. Selections 
+#can be cancelled by right-clicking the plot. This program assumes that the first click indicates the starting 
+#time, the second click indicates the ending time and the third click closes the plot.
 #The selections are marked with red cross.
-#It is recommended to click between the measurement points and not the points
-#themselves to make sure the selected interval is what is wanted (all the points
-#located between the red crosses are included for the fitting).
+#It is recommended to click between the measurement points, rather than the points themselves, to ensure the 
+#selected interval is what is wanted (all points located between the red crosses are included in the fitting).
 #If the parameter is False, then the starting and ending times in the
 #times_file are used to make the fits and calculate the fluxes.
 interactive_times=True
 
 #===============================================================================================================#
 
-#Replace the starting and ending times in the times file with the
-#interactively selected times (True). The new TIMES file is saved
-#in the folder indicated by the results_loc parameter below.
-#The old times file is not replaced! The new file will have a suffix
-#"_new" to avoid overwriting.
-#If False, the interactively selected times will not be saved, but
-#they will still be used in the flux calculation.
+#Save the new interactively selected starting and ending times in the new times file (True)
+
+#The new TIMES file is saved in the folder indicated by the results_loc parameter below.
+#The old times file is not replaced! The new file will have a suffix "_new" to avoid overwriting.
+#If False, the interactively selected times will not be saved, but they will still be used in the flux 
+#calculation.
 interactive_save=True
 
 #===============================================================================================================#
 
-#How many seconds of data will be discarded from the start of the measurement.
+#How many seconds of data will be discarded from the start of the measurement
+
 #This will be applied also when selecting the starting times interactively.
-#If set to 0, the starting time will be exactly the one selected interactively 
-#or the one given in the times-file.
+#If set to 0, the starting time will be exactly the one selected interactively or the one given in the 
+#times-file.
 skip_start=0
 
 #===============================================================================================================#
 
-#The minimum and the maximum allowed length of the fit in seconds.
-#If the fit is longer than fit_maximum, datapoints from the end will
-#be removed until the length is less than fit_maximum
+#The minimum and the maximum allowed length of the fit in seconds
+
+#If the fit is longer than fit_maximum, datapoints from the end will be removed until the length is less than 
+#fit_maximum
 #If the fit is shorter than fit_minimum, the closure will be ignored
 fit_minimum=60
 fit_maximum=330
@@ -151,24 +158,29 @@ fit_maximum=330
 #===============================================================================================================#
 
 #Use time averaging in the mixing ratio data.
+
 #In seconds. Set to zero if no time averaging is wanted.
 time_avg=5
 
 #===============================================================================================================#
 
 #Plot closures? (True or False)
-#If True, plot the mixing ratio time series of all the selected gases and
-#the fits made into them and save the figures.
+
+#If True, plot the mixing ratio time series of all the selected gases and the fits made into them and save the 
+#figures.
 plot_closures=True
 
 #===============================================================================================================#
 
-#Save also the fitting statistics (True) or just the fluxes and the optional
-#columns in the data file (False)
+#Save also the fitting statistics (True) or just the fluxes and the optional columns in the data file (False)
+
 #The statistics will be calculated for all the gases and for both linear and exponential fluxes.
 #The calculated statistics are: standard deviation of the slope of the fit (e.g. std_CO2_lin), normalized root
 #mean square error of the fit (NRMSE), adjusted R2 value of the fit (R2adj), Akaike information criterion of the
 #fit (AIC), autocorrelation of 1 point lag (autocorr)
+#People are usually not interested in these, which is why there is an option not to save them to avoid bloating 
+#the result file.
+
 save_statistics=True
 
 #===============================================================================================================#
